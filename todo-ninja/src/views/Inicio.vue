c76c3820a8ad0f1a333492845791d45b9de3e918
Manubilo <mjdelgado@pucp.pe>
2019-09-25 00:43:03 -0500

    agregado patan lol

--- todo-ninja/src/views/Inicio.vue
+++ todo-ninja/src/views/Inicio.vue
@@ -3,6 +3,15 @@
     <h1 class="subheading grey--text">Página principal</h1>
 
     <v-container>
+
+        <v-avatar size="300">
+
+        <img
+            src="../assets/patan.jpg"
+        >
+        
+        </v-avatar>
+
         <p class="red--text">Esta es la pagina principal wii</p>
 
     </v-container>
